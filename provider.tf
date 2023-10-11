6d0794bb61fa5a7e0c53960609412f5d2585b464
stelazh <stela.zh@gmail.com>
2023-10-11 20:40:39 +0200

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "azurermaaa" {
+  features = {}
+  client_id       = "51ab98ae-9a15-4d20-b9ee-995b45fa0db9"
+  client_secret   = "Ejb8Q~GnRU_iUXJyC2PBMYCrh5cNkPaYX8GSxb2i"
+  tenant_id       = "75b23eb2-6e15-4a4f-8062-94ac49d0d28c"
+  subscription_id = "884a4ad0-93d7-4805-a216-1a861a07904b"
+}
